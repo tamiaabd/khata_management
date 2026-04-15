b32f99f9e1496070b0ade7509f716a7b9d7955ce
tamiaabd <tamiaabdulmalik1@gmail.com>
2026-04-15 16:54:59 +0500

    Your commit message

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
 
 abstract final class AppColors {
-  static const Color primary = Color(0xFF2D5A5A);
-  static const Color primaryLight = Color(0xFFE8F0F0);
-  static const Color background = Color(0xFFF8F9FA);
+  static const Color primary = Color(0xFF2196F3);
+  static const Color primaryLight = Color(0xFFE3F2FD);
+  static const Color background = Color(0xFFF5FAFF);
   static const Color paper = Color(0xFFFFFFFF);
-  static const Color textPrimary = Color(0xFF1A1A1A);
-  static const Color textSecondary = Color(0xFF6B7280);
-  static const Color gridLine = Color(0xFFE5E7EB);
-  static const Color gridLineLight = Color(0xFFF3F4F6);
+  static const Color textPrimary = Color(0xFF0D1B2A);
+  static const Color textSecondary = Color(0xFF5C7EA6);
+  static const Color gridLine = Color(0xFFD6EAFB);
+  static const Color gridLineLight = Color(0xFFEAF4FF);
   static const Color delete = Color(0xFFDC2626);
 }
 
